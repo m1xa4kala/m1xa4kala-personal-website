50154539fe8f0947c214654fb8e4c2884e1d31f3
m1xa4kala <maikl2020011@gmail.com>
2022-06-18 23:56:39 +0300

    remove react imports

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Layout from '../components/layouts/Main'
 
 const MyApp = ({ router, Component, pageProps }) => {
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Document, { NextScript, Head, Html, Main } from 'next/document'
 
 class Website extends Document {
