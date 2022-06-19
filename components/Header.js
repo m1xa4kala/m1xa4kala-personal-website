2b70e58ea5b7bd2735ac0f3cb3ebd905a357e923
m1xa4kala <maikl2020011@gmail.com>
2022-06-19 20:53:18 +0300

    add header styles

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
-import { HStack, Link, Flex } from '@chakra-ui/react'
+import { HStack, Link, Flex, Box, useColorModeValue } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Logo from './Logo'
 
 const Header = props => {
   const { path } = props
   return (
-    <header>
+    <Box
+      as='header'
+      position='fixed'
+      p={2}
+      bg={useColorModeValue('ffffff40', '20202380')}
+      w='full'
+      backdropFilter={'blur(10px)'}
+      zIndex={10}
+    >
       <Flex alignItems='center' justifyContent='space-around'>
         <Logo />
         <nav>
@@ -16,7 +24,7 @@ const Header = props => {
           </HStack>
         </nav>
       </Flex>
-    </header>
+    </Box>
   )
 }
 
