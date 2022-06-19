7ea2a20389bcf362656232ad330fcf40bb6d956b
m1xa4kala <maikl2020011@gmail.com>
2022-06-19 20:54:07 +0300

    add chakra provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/Main'
+import theme from '../lib/theme'
 
 const MyApp = ({ router, Component, pageProps }) => {
   return (
-    <Layout router={router}>
-      <Component {...pageProps} key={router.route} />
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+    </ChakraProvider>
   )
 }
 
