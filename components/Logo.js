1db13084f3be605b843dde42104ac3146f3486d6
m1xa4kala <maikl2020011@gmail.com>
2022-06-22 16:23:11 +0300

    add size prop

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,11 @@
 import NextLink from 'next/link'
 import { Heading } from '@chakra-ui/react'
 
-const Logo = () => {
+const Logo = ({ size = 'md' }) => {
   return (
     <NextLink href='/'>
       <a>
-        <Heading size='md' fontWeight={500}>
+        <Heading size={size} fontWeight={500}>
           Mikhail Khaybullin
         </Heading>
       </a>
