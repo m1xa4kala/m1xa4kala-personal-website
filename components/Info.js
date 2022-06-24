86e98c0ebd95c29469a4c93c8ca65cf1d7016214
m1xa4kala <maikl2020011@gmail.com>
2022-06-24 17:07:57 +0300

    add Info component

---
+++ components/Info.js
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react"
+
+const Info = () => {
+  return (
+  <Box>
+    <Heading>Mikhail Khaybullin</Heading>
+      <Text>Junior Frontend developer</Text>
+    </Box>
+)
+}
+
+export default Info
