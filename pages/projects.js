334c6fbe124aa97df37a81ff12181d39cd3f7d2e
m1xa4kala <maikl2020011@gmail.com>
2022-06-24 11:59:58 +0300

    add projects page

---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layouts/Article'
+import { Heading } from '@chakra-ui/react'
+
+const projects = () => {
+  return (
+    <Layout title='Projects'>
+      <Heading>Projects</Heading>
+    </Layout>
+  )
+}
+
+export default projects
