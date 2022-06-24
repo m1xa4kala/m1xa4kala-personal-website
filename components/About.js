b8b4da6dcf9a1fcaee86a6b89ffed019a26afb81
m1xa4kala <maikl2020011@gmail.com>
2022-06-24 17:08:25 +0300

    add About component

---
+++ components/About.js
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/react'
+
+const About = () => {
+  return (
+  <Text as='p' fontSize='xl'>
+      Hello, I&apos;m Frontend developer from Russia!
+<br/>
+      For the past few years I have improving my skill in program engineering, exploring computer science and awesome frontend stuff.
+    </Text>
+)
+}
+
+export default About
